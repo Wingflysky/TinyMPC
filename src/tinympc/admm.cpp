aae5334d6dddc4adcd37c1c5a2a467e92a1b25fb
Anoushka Alavilli <anoushkaalavill@anoushkas-mbp-2.wifi.local.cmu.edu>
2023-08-30 16:12:42 -0400

    Add Julia interfacing for ADMM alg

--- src/tinympc/admm.cpp
+++ src/tinympc/admm.cpp
@@ -108,21 +108,106 @@ void julia_sim_wrapper_solve_lqr(float x[12][10], float u[4][9]){
     Eigen::Map<tiny_MatrixNuNhm1> problem_u(&u[0][0], Nu, Nh-1);
     problem.u = problem_u;
 
-    std::cout << params.Xref << std::endl;
+    // std::cout << params.Xref << std::endl;
 
-    solve_admm(&problem, &params);
+    solve_lqr(&problem, &params);
     Eigen::Map<tiny_MatrixNuNhm1>(&u[0][0], Nu, Nh-1) = problem.u;
 
-    std::cout << problem.iter << std::endl;
-    std::cout << problem.u.col(0)(0) << std::endl;
-    std::cout << problem.u.col(0)(1) << std::endl;
-    std::cout << problem.u.col(0)(2) << std::endl;
+    // std::cout << problem.iter << std::endl;
+    std::cout << "LQR RESULTS ";
+    std::cout << problem.u.col(0)(0) << " ";
+    std::cout << problem.u.col(0)(1) << " ";
+    std::cout << problem.u.col(0)(2) << " ";
     std::cout << problem.u.col(0)(3) << std::endl;
 
 }
 
 void solve_lqr(struct tiny_problem *problem, const struct tiny_params *params) {
     problem->u.col(0) = -params->cache.Kinf * (problem->x.col(0) - params->Xref.col(0));
+}
+
+void julia_sim_wrapper_solve_admm(float x[12][10], float u[4][9]){
+    // Copy data from problem_data/quadrotor*.hpp
+    struct tiny_cache cache;
+    cache.Adyn = Eigen::Map<Matrix<tinytype, NSTATES, NSTATES, Eigen::RowMajor>>(Adyn_data);
+    cache.Bdyn = Eigen::Map<Matrix<tinytype, NSTATES, NINPUTS, Eigen::RowMajor>>(Bdyn_data);
+    cache.rho = rho_value;
+    cache.Kinf = Eigen::Map<Matrix<tinytype, NINPUTS, NSTATES, Eigen::RowMajor>>(Kinf_data);
+    cache.Pinf = Eigen::Map<Matrix<tinytype, NSTATES, NSTATES, Eigen::RowMajor>>(Pinf_data);
+    cache.Quu_inv = Eigen::Map<Matrix<tinytype, NINPUTS, NINPUTS, Eigen::RowMajor>>(Quu_inv_data);
+    cache.AmBKt = Eigen::Map<Matrix<tinytype, NSTATES, NSTATES, Eigen::RowMajor>>(AmBKt_data);
+    cache.coeff_d2p = Eigen::Map<Matrix<tinytype, NSTATES, NINPUTS, Eigen::RowMajor>>(coeff_d2p_data);
+
+    struct tiny_params params;
+    params.Q = Eigen::Map<tiny_VectorNx>(Q_data);
+    params.Qf = Eigen::Map<tiny_VectorNx>(Qf_data);
+    params.R = Eigen::Map<tiny_VectorNu>(R_data);
+    params.u_min = tiny_MatrixNuNhm1::Constant(-0.5);
+    params.u_max = tiny_MatrixNuNhm1::Constant(0.5);
+    for (int i=0; i<NHORIZON; i++) {
+        params.x_min[i] = tiny_VectorNc::Constant(-99999); // Currently unused
+        params.x_max[i] = tiny_VectorNc::Zero();
+        params.A_constraints[i] = tiny_MatrixNcNx::Zero();
+    }
+    params.Xref = tiny_MatrixNxNh::Zero();
+    params.Uref = tiny_MatrixNuNhm1::Zero();
+    params.cache = cache;
+
+    struct tiny_problem problem;
+    problem.x = tiny_MatrixNxNh::Zero();
+    problem.q = tiny_MatrixNxNh::Zero();
+    problem.p = tiny_MatrixNxNh::Zero();
+    problem.v = tiny_MatrixNxNh::Zero();
+    problem.vnew = tiny_MatrixNxNh::Zero();
+    problem.g = tiny_MatrixNxNh::Zero();
+
+    problem.u = tiny_MatrixNuNhm1::Zero();
+    problem.r = tiny_MatrixNuNhm1::Zero();
+    problem.d = tiny_MatrixNuNhm1::Zero();
+    problem.z = tiny_MatrixNuNhm1::Zero();
+    problem.znew = tiny_MatrixNuNhm1::Zero();
+    problem.y = tiny_MatrixNuNhm1::Zero();
+
+    problem.primal_residual_state = 0;
+    problem.primal_residual_input = 0;
+    problem.dual_residual_state = 0;
+    problem.dual_residual_input = 0;
+    problem.abs_tol = 0.001;
+    problem.status = 0;
+    problem.iter = 0;
+    problem.max_iter = 100;
+    problem.iters_check_rho_update = 10;
+
+    // Copy reference trajectory into Eigen matrix
+    // Matrix<tinytype, NSTATES, NTOTAL, Eigen::ColMajor> Xref_total = Eigen::Map<Matrix<tinytype, NTOTAL, NSTATES, Eigen::RowMajor>>(Xref_data).transpose();
+    Matrix<tinytype, NSTATES, 1> Xref_origin;
+    Xref_origin << 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+
+    // params.Xref = Xref_total.block<NSTATES, NHORIZON>(0,0);
+    params.Xref = Xref_origin.replicate<1,NHORIZON>();
+    // problem.x.col(0) = params.Xref.col(0);
+    problem.x.col(0) << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+
+    int Nx = 12;
+    int Nh = 10;
+    int Nu = 4;
+    Eigen::Map<tiny_MatrixNxNh> problem_x(&x[0][0], Nx, Nh);
+    problem.x = problem_x;
+    Eigen::Map<tiny_MatrixNuNhm1> problem_u(&u[0][0], Nu, Nh-1);
+    problem.u = problem_u;
+
+    // std::cout << params.Xref << std::endl;
+
+    solve_admm(&problem, &params);
+    Eigen::Map<tiny_MatrixNuNhm1>(&u[0][0], Nu, Nh-1) = problem.u;
+
+    std::cout << "ADMM RESULTS ";
+    std::cout << problem.iter << std::endl;
+    std::cout << problem.u.col(0)(0) << " ";
+    std::cout << problem.u.col(0)(1) << " ";
+    std::cout << problem.u.col(0)(2) << " ";
+    std::cout << problem.u.col(0)(3) << std::endl;
+
 }
 
 void solve_admm(struct tiny_problem *problem, const struct tiny_params *params) {
@@ -148,10 +233,10 @@ void solve_admm(struct tiny_problem *problem, const struct tiny_params *params) 
         // Update linear control cost terms using reference trajectory, duals, and slack variables
         update_linear_cost(problem, params);
 
-        // problem->primal_residual_state = (problem->x - problem->vnew).cwiseAbs().maxCoeff();
-        // problem->dual_residual_state = ((problem->v - problem->vnew).cwiseAbs().maxCoeff()) * params->cache.rho;
-        // problem->primal_residual_input = (problem->u - problem->znew).cwiseAbs().maxCoeff();
-        // problem->dual_residual_input = ((problem->z - problem->znew).cwiseAbs().maxCoeff()) * params->cache.rho;
+        problem->primal_residual_state = (problem->x - problem->vnew).cwiseAbs().maxCoeff();
+        problem->dual_residual_state = ((problem->v - problem->vnew).cwiseAbs().maxCoeff()) * params->cache.rho;
+        problem->primal_residual_input = (problem->u - problem->znew).cwiseAbs().maxCoeff();
+        problem->dual_residual_input = ((problem->z - problem->znew).cwiseAbs().maxCoeff()) * params->cache.rho;
 
         // TODO: convert arrays of Eigen vectors into one Eigen matrix
         // Save previous slack variables
@@ -160,14 +245,14 @@ void solve_admm(struct tiny_problem *problem, const struct tiny_params *params) 
 
         // TODO: remove convergence check and just return when allotted runtime is up
         // Check for convergence
-        // if (problem->primal_residual_state < problem->abs_tol &&
-        //     problem->primal_residual_input < problem->abs_tol &&
-        //     problem->dual_residual_state < problem->abs_tol &&
-        //     problem->dual_residual_input < problem->abs_tol)
-        // {
-        //     problem->status = 1;
-        //     break;
-        // }
+        if (problem->primal_residual_state < problem->abs_tol &&
+            problem->primal_residual_input < problem->abs_tol &&
+            problem->dual_residual_state < problem->abs_tol &&
+            problem->dual_residual_input < problem->abs_tol)
+        {
+            problem->status = 1;
+            break;
+        }
 
         // TODO: add rho scaling
 
